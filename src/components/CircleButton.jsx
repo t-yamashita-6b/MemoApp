a50f4eefd976e55733a0f4cdfceefab96b1fc63f
yamac <t.yamashita.6b@gmail.com>
2022-01-03 17:25:14 +0900

    add styles

---
+++ src/components/CircleButton.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function CicleButton() {
+  return(
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>+</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#FFFFFF',
+    fontSize: 40,
+    lineHeight: 40,
+  },
+});
+
